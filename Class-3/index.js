6647b57190d6566ce20f193de70f1f8ce3e1acca
engineerpraveen <vaishnav.praveen001@gmail.com>
2025-03-25 13:02:51 +0530

    Create frontend and add forms

--- Class-3/index.js
+++ Class-3/index.js
@@ -1,10 +1,7 @@
 const express = require('express')
 const fs = require('fs');
-
 const PORT = 8002;
-
 const app = express();
-
 const products = require("./MOCK_DATA.json");
 
 // Middleware : inbuilt
@@ -24,7 +21,6 @@ app.get("/", (req, res) => {
 //     res.json(product);
 // })
 
-
 // this is called server side render : In server side rendering js runs on server and only send html structure to client
 app.get('/api/products', (req, res) => {
     const html = `<ul> ${products.map(items => `<li>${items.product_name}</li>`)}</ul>`
@@ -37,8 +33,6 @@ app.get('/api/products/:id', (req, res) => {
     const product = products.find(product => product.id === id);
     res.send(product);
 })
-
-
 
 // post Method for creation
 app.post('/api/products', (req, res) => {
@@ -59,7 +53,6 @@ app.put('/api/products/:id', (req, res)=>{
     const body = req.body;
     const productIndex = products.findIndex(product => product.id === id);
     products[productIndex] = {id: Number(id), ...body}
-
     fs.writeFile('./MOCK_DATA.json', JSON.stringify(products), (err, data)=>{
         if (err) {
             return res.status(500).json({status:'error', message:"error"})
@@ -73,7 +66,6 @@ app.delete('/api/products/:id', (req,res)=>{
     const id = Number(req.params.id);
     const productIndex = products.findIndex(product => product.id === id);
     products.splice(productIndex, 1)
-
     fs.writeFile('./MOCK_DATA.json', JSON.stringify(products), (err, data)=>{
         if(err){
             return res.status(500).json({status:"error", message:"error"})
